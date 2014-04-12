b77af576540eb4eaebb30df3dde5a740de5085db
dpb15 <brannerchinese@gmail.com>
2014-04-12 14:26:28 -0400

    add utils file to import api_key w/o revealing

--- CODE/utils.py
+++ CODE/utils.py
@@ -4,8 +4,13 @@
 
 """Utilities for Weather Study project."""
 
-with open() as f:
-    api_key = 
+import os
+
+filename = 'weather_underground_api.ignore'
+with open(os.path.join('../DATA', filename), 'r') as f:
+    api_key = f.read()
+
+print(api_key)
 
 def construct_WU_api_req(city='New_York', history=None):
     head = 'http://api.wunderground.com/api/'
