3c72b061d115e958550e1f7a279890599990bbb3
dpb15 <brannerchinese@gmail.com>
2014-04-12 22:53:54 -0400

    update notes and add utils.py; delete temp.dat

--- CODE/utils.py
+++ CODE/utils.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 # David Prager Branner
-# 20140411, abandoned; does not compile
+# 20140412
 
 """Utilities for Weather Study project."""
 
@@ -12,6 +12,7 @@ with open(os.path.join('../DATA', filename), 'r') as f:
 
 print(api_key)
 
+# The following is not working yet.
 def construct_WU_api_req(city='New_York', history=None):
     head = 'http://api.wunderground.com/api/'
     tail = '/geolookup/conditions/q/'
---
+++ REFERENCE/weather_underground_notes_20140412.md
@@ -0,0 +1,7 @@
+## Weather Underground Notes
+
+[edited 20140412]
+
+1. Code added to `utils.py` to read API-key from file (ignored by GitHub) so that it can be incorporated into API-requests without revealing it.
+
+[end]
