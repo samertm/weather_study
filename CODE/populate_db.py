dc79c70a1ba720be7e9dd4b712be7483143b5d25
Gina Schmalzle <ginaschmalzle@gmail.com>
2014-04-19 18:16:22 -0400

    Edit populate_db_w_forecasts not working

--- CODE/populate_db.py
+++ CODE/populate_db.py
@@ -19,14 +19,31 @@ import tarfile
 import pprint
 import utils as U
 
-def populate_db_w_forecasts(forecast_dict):
+def populate_db_w_forecasts(forecast_dict, db='weather_data_OWM.db'):
     """Populate database with the contents of a forecast dictionary."""
-    pass
+    connection = sqlite3.connect(os.path.join('../', db))
+    with connection:
+    	cursor = connection.cursor()
+    	for key in forecast_dict:
+    		if key == 'query_date':
+    			continue
+    		for i,item in enumerate(forecast_dict[key]):
+   				target_date = U.convert_from_unixtime(int(item[0]))
+   				target_date = target_date.split('-')[0]
+   				maxt, mint, rain, snow = item[1:]
+   				i = str(i)
+   				fields = ('maxt_'+i, 'mint_'+i, 'rain_'+i, 'snow_'+i)
+   				cursor.execute(
+						'''INSERT INTO forecasts''' + str(fields) +
+						'''VALUES (?,?,?,?) ''' + 
+						''' WHERE location_id=?,target_date=?''',
+						(maxt, mint, rain, snow, key, target_date) 
+				)
 
 def process_dir_of_downloads(to_print=None):
     """Populate database with the forecasts from all files in DOWNLOADS."""
     # Get names of directories in download folder
-    directories = open_directory('../DOWNLOADS/downloads_OWM_US_')
+    directories = U.open_directory('../DOWNLOADS/downloads_OWM_US_')
     # For each directory, get all files
     for directory in directories:
         print(directory) # debug
@@ -48,4 +65,3 @@ def populate_db_w_city_codes(db='weather_data_OWM.db'):
             cursor.execute(
                     '''INSERT INTO locations VALUES''' +
                     str(tuple(code)))
-            
