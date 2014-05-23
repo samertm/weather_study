a4e7782e584ece74e91601f7603a21e30b7c8c32
David Branner <brannerchinese@gmail.com>
2014-05-23 15:26:37 -0400

    update shell script

--- shell_scripts/run_weather_requests.sh
+++ shell_scripts/run_weather_requests.sh
@@ -6,6 +6,6 @@
 cd /home/dpb/github_public/weather_study
 source v_env3/bin/activate
 cd CODE
-/home/dpb/github_public/weather_study/CODE/requests.py
-git-add -Av ..
+python /home/dpb/github_public/weather_study/CODE/requests.py
+git-add -A ..
 git-commit -m 'upload downloads from today'
