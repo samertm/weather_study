209ec006dd80a8ddf147c7301cb0286ba9a5061f
David Branner <brannerchinese@gmail.com>
2014-05-23 14:50:08 -0400

    update script

--- shell_scripts/run_weather_requests.sh
+++ shell_scripts/run_weather_requests.sh
@@ -4,13 +4,8 @@
 # 20140523
 
 cd /home/dpb/github_public/weather_study
-echo `pwd`
 source v_env3/bin/activate
 cd CODE
-echo `pwd`
-python -c 'print("here within Python")'
 /home/dpb/github_public/weather_study/CODE/requests.py
-echo 'done with python'
-git add -Av ..
-git commit -m 'upload downloads from today'
-echo 'done with git'
+git-add -Av ..
+git-commit -m 'upload downloads from today'
