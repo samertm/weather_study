9d47d4669ec8a46edf453d35c4ad4aae23ba2ad4
David Branner <brannerchinese@gmail.com>
2014-05-23 10:58:38 -0400

    add shell script

---
+++ shell_scripts/run_weather_requests.sh
@@ -0,0 +1,12 @@
+# run_weather_requests.sh
+# Run weather study's `requests.py` within the master branch.
+# David Prager Branner
+# 20140523
+
+cd /home/dpb/github_public/weather_study
+git checkout master
+source v_env3/bin/activate
+cd CODE
+python requests.py
+git add -Av ..
+git commit -m 'upload downloads from today'
