febdd16d700fb81f506c65842cc7423dc45efdc0
dpb15 <brannerchinese@gmail.com>
2014-05-31 23:47:05 -0400

    change all dirs to lower case with _

--- OUTPUT/js/basemap.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map_canvas').setView([40, -90.], 4);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-     maxZoom: 20
-	}).addTo(map);
-  
