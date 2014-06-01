fe08877d098d3dd77c98e71c6c600c44773eb94b
dpb15 <brannerchinese@gmail.com>
2014-06-01 13:10:51 -0400

    temporarily rename top-level dirs, adding _

--- shell_scripts_/run_weather_requests.sh
+++ shell_scripts_/run_weather_requests.sh
@@ -1,11 +1,11 @@
 # run_weather_requests.sh
 # Run weather study's `requests.py` within the master branch.
 # David Prager Branner
-# 20140523
+# 20140601
 
 cd /home/dpb/github_public/weather_study
 source v_env3/bin/activate
-cd CODE
-python /home/dpb/github_public/weather_study/CODE/requests.py
+cd code
+python /home/dpb/github_public/weather_study/code/requests.py
 git add -A /home/dpb/github_public/weather_study
 git commit -m 'upload downloads from today'
